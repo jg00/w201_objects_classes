5316e2a661664dfbc430ec0aae26b88b23fe40ce
Jack Gamboa <jgamboa929@gmail.com>
2018-09-25 00:01:37 -0500

    Updated to do list with user input command prompts

--- assn1_todo_list.py
+++ assn1_todo_list.py
@@ -1,18 +1,18 @@
-
-
+# To Do list - Initially prompts to create a user.  It then prompts user to add tasks.
 
 class Task:
     def __init__(self, task_name):
         self.task_name = task_name
-        self.priority = "priority: none"   #default
+        self.priority = "priority: none"
+
 
 class User:
     def __init__(self, name):
         self.name = name
         self.tasks = []
 
-    def add_task(self, task_name):
-        self.tasks.append(task_name)
+    def add_task_object(self, task_name):
+        self.tasks.append(Task(task_name))
 
     def view_tasks(self):
         print(self.name + "\'s tasks:")
@@ -30,20 +30,52 @@ class User:
                 del self.tasks[index]
 
 
-jack = User('Jack')
+def create_user():
+    continue_prompt = True
 
-mow = Task('mow')
-laundry = Task('laundry')
-groceries = Task('groceries')
+    while continue_prompt:
+        user = input("Create a user: ")
 
-jack.add_task(laundry)
-jack.add_task(groceries)
-jack.add_task(mow)
+        if user.isalpha():
+            continue_prompt = False
+            return User(user)
 
-jack.set_task_priority('groceries', 'high')
 
-jack.view_tasks()
+def task_commands(user):
+    while True:
+        prompt = input("Enter task command (a-add v-view r-remove p-setpriority q-exit): ")
 
-jack.remove_task('mow')
+        if (prompt == "a"):
+            task_name = input("Enter task name: ")
+            user.add_task_object(task_name)
 
-jack.view_tasks()
+        elif (prompt == "v"):
+            user.view_tasks()
+
+        elif (prompt == "p"):
+            task_name_prompt = input("Enter existing task name to set priority: ")
+            priority_value_prompt = input("Enter priority for the task: ")
+            user.set_task_priority(task_name_prompt, priority_value_prompt)
+
+        elif (prompt == "r"):
+            task_name_prompt = input("Enter existing task name to remove: ")
+            user.remove_task(task_name_prompt)
+
+        elif (prompt == "q"):
+            break
+
+        else:
+            print("Invalid task command")
+
+
+continue_app = True
+print("# To Do List App Started #")
+while continue_app:
+
+    user = create_user()
+
+    task_commands(user)
+
+    print("# To Do List App Ended #")
+    continue_app = False
+
